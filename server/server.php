dc9d553f5d02e659bc0a9b349f3b7b660afcc09e
cmichelutti99 <64210471+cmichelutti99@users.noreply.github.com>
2020-05-22 18:45:23 +0200

    Update server.php

--- server/server.php
+++ server/server.php
@@ -11,10 +11,10 @@ $edit_state = false;
 $db = mysqli_connect('localhost', 'root', '', 'TBD');
 
 if (isset($_POST['add'])){
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
+    $firstname = $_POST['first_name'];
+    $lastname = $_POST['last_name'];
     
-    $query = "INSERT INTO Patient (firstname, lastname) VALUES ('$firstname', '$lastname')";
+    $query = "INSERT INTO patients (first_name, last_name) VALUES ('$firstname', '$lastname')";
     
     mysqli_query($db, $query);
     $_SESSION['msg'] = "Patient saved";
@@ -23,23 +23,23 @@ if (isset($_POST['add'])){
 
 if (isset($_POST['update'])){
     
-    $firstname = mysqli_real_escape_string($_POST['firstname']);
-    $lastname = mysqli_real_escape_string($_POST['lastname']);
+    $firstname = mysqli_real_escape_string($_POST['first_name']);
+    $lastname = mysqli_real_escape_string($_POST['last_name']);
     $id = mysqli_real_escape_string($_POST['id']);
     
-    mysqli_query($db, "UPDATE Patient SET firstname = '$firstname', lastname ='$lastname' WHERE id=$id");
+    mysqli_query($db, "UPDATE patients SET first_name = '$firstname', last_name ='$lastname' WHERE id=$id");
     $_SESSION['msg'] = "Patient updated";
     header('location: index.php');
 }
 
 if (isset($_GET['del'])) {
     $id = $_GET['del'];
-    mysqli_query($db, "DELETE FROM Patient WHERE id=$id");
+    mysqli_query($db, "DELETE FROM patients WHERE id=$id");
     $_SESSION['msg'] = "Patient deleted";
     header('location: index.php');
 }
 
-$results = mysqli_query($db, "SELECT * FROM Patient");
+$results = mysqli_query($db, "SELECT * FROM patients");
 
 
 
