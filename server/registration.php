e92207c5bbf6c50fc99544d4664b0043130d4f0e
cmichelutti99 <64210471+cmichelutti99@users.noreply.github.com>
2020-05-23 18:34:49 +0200

    Update registration.php

--- server/registration.php
+++ server/registration.php
@@ -1,28 +1,24 @@
-<?php
-
+<?php  
 session_start();
 
 $con=mysqli_connect('localhost', 'root','');
 
 mysqli_select_db($con, 'TBD');
+    
+    $first_name=$_POST['first_name'];
+    $last_name=$_POST['last_name'];
+    $email=$_POST['email'];
+    $user_name=$_POST['user_name'];
+    $password = $_POST['password'];
+    
+    $sql = "INSERT INTO `doctors` (`id`, `first_name`, `last_name`, `user_name`, `password`, `ip_addr`, `email`) VALUES (NULL, '$first_name', '$last_name', '$user_name', '$password', '', '$email')"; 
 
-$user_name=$_POST['user_name'];
-$password = $_POST['password'];
-
-$s = "SELECT * FROM doctors WHERE user_name='$user_name'";
-
-$result = mysqli_query($con, $s);
-
-$num = mysqli_num_rows($result);
-
-if($num == 1){
-    echo "Username already taken";
-
-}else{
-    $reg= "INSERT INTO doctors(user_name, password) VALUES ('$user_name', '$password')";
-    mysqli_query($con, $reg);
-    echo "Registration Successful";
-}
-
-
-?>
+    
+    if ($con->query($sql) === TRUE) {  
+        header('Location: login.php');  
+    } else {  
+        echo "Error: " . $sql . "<br>" . $con->error;  
+    }  
+    $con->close();  
+  
+?> 
