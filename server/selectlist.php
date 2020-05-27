ac4c4f6f8ed19f254ffacaa0c16b8a3600398f3b
cmichelutti99 <64210471+cmichelutti99@users.noreply.github.com>
2020-05-27 16:49:39 +0200

    Create selectlist.php

---
+++ server/selectlist.php
@@ -0,0 +1,66 @@
+<?php include('server.php'); 
+
+if(isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $edit_state = true;
+    $rec = mysqli_query($db, "SELECT * FROM Patient WHERE id=$id");
+    $record = mysqli_fetch_array($rec);
+    $firstname = $record['firstname'];
+    $lastname = $record['lastname'];
+    $fiscalcode = $record['fiscalcode'];
+    $id = $record['id'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Select patient</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <form action="dashboard.php" method="post">
+        <button type="submit" name="back" class="btn">Back</button>
+        </form>
+        
+        <?php if(isset($_SESSION['msg'])): ?>
+            <div class="msg">
+                <?php
+                    echo $_SESSION['msg'];
+                    unset($_SESSION['msg']);
+                ?>
+            </div>
+        <?php endif?>
+        
+        
+        <table>
+        <thead>
+            <tr>
+                <th>Select</th>
+                <th>ID</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Fiscal code</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = mysqli_fetch_array($results)){?>
+                <tr>
+                    <td>
+                        <?php echo '<a href="appointment.php?id='.$row['id'].'">'Select'</a>'; ?>
+                    </td>
+                    <td><?php echo $row['id']?></td>
+                    <td><?php echo $row['first_name']?></td>
+                    <td><?php echo $row['last_name']?></td>
+                    <td><?php echo $row['fiscal_code']?></td>
+                </tr>
+           <?php }?>
+           
+        </tbody>
+        </table>
+        <form action="logout.php" method="post">
+        <button type="submit" name="logout" class="btn">Logout</button>
+        </form>
+    </body>
+</html>
