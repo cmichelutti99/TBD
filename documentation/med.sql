f83fc18f863704debb91eb2d873efe5385808d80
ThomasSchwaab <thomas@schwaab.bayern>
2020-06-29 16:03:31 +0200

    -add latest db sql file

--- documentation/med.sql
+++ documentation/med.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2020 at 11:11 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Erstellungszeit: 29. Jun 2020 um 16:02
+-- Server-Version: 10.4.11-MariaDB
+-- PHP-Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,38 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `med`
+-- Datenbank: `med`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `doctors`
+-- Tabellenstruktur für Tabelle `appoint`
+--
+
+CREATE TABLE `appoint` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `did` int(11) NOT NULL,
+  `measure` varchar(30) NOT NULL,
+  `notes` text NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `appoint`
+--
+
+INSERT INTO `appoint` (`id`, `uid`, `did`, `measure`, `notes`, `date`) VALUES
+(1, 2, 3, 'Tooth repair', '-24 new tooths\r\nEverything else\r\n\r\nPatien didn\'t pay last time within 2 weeks!\r\n', '0000-00-00'),
+(2, 2, 3, 'Tooth Cleaning', 'Test123', '0000-00-00'),
+(3, 2, 3, 'Examination', 'Test 112323', '2020-07-02'),
+(4, 2, 3, 'other', 'Test32423434', '2020-06-11');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `doctors`
 --
 
 CREATE TABLE `doctors` (
@@ -38,23 +63,19 @@ CREATE TABLE `doctors` (
   `email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `measure`
+-- Daten für Tabelle `doctors`
 --
 
-CREATE TABLE `measure` (
-  `date_time` datetime(6) NOT NULL,
-  `temp` float NOT NULL,
-  `notes` varchar(200) NOT NULL,
-  `patient_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `doctors` (`id`, `first_name`, `last_name`, `user_name`, `password`, `ip_addr`, `email`) VALUES
+(1, 'Peter', 'Fuchs', 'pfuchs', 'fdd39291e70a6782c53272bb3eb31692', '123.32.54.22', 'pfuchs@web.de'),
+(3, 'Thomas', 'Schwaab', 'tschwaab', '1811d2606ceb8a3511844d2dd6fe045e', '192.168.123.1', 'thomas@schwaab.bayern'),
+(4, 'Peter', 'Michael', 'mey', '1811d2606ceb8a3511844d2dd6fe045e', '::1', 'pmichal@rofl.de');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `patients`
+-- Tabellenstruktur für Tabelle `patients`
 --
 
 CREATE TABLE `patients` (
@@ -62,54 +83,61 @@ CREATE TABLE `patients` (
   `first_name` varchar(25) NOT NULL,
   `last_name` varchar(25) NOT NULL,
   `fiscal_code` varchar(30) NOT NULL,
-  `ip_addr` varchar(25) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `doctor_id` int(11) NOT NULL
+  `email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Daten für Tabelle `patients`
+--
+
+INSERT INTO `patients` (`id`, `first_name`, `last_name`, `fiscal_code`, `email`) VALUES
+(2, 'Meier', 'Bach', '96969', ''),
+(4, 'Karl', 'Blala', '22312323', 'wewewee'),
+(5, 'Karl', 'Blala', '22312323', 'wewewee');
+
+--
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `doctors`
+-- Indizes für die Tabelle `appoint`
+--
+ALTER TABLE `appoint`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `doctors`
 --
 ALTER TABLE `doctors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `measure`
---
-ALTER TABLE `measure`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `patients`
+-- Indizes für die Tabelle `patients`
 --
 ALTER TABLE `patients`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `doctors`
+-- AUTO_INCREMENT für Tabelle `appoint`
+--
+ALTER TABLE `appoint`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT für Tabelle `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `measure`
---
-ALTER TABLE `measure`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `patients`
+-- AUTO_INCREMENT für Tabelle `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
